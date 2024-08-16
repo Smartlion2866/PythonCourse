b57c19a8feb0b6f314d515377bc78e60f449bcb8
Smartlion2866 <miguel.begazo3716@gmail.com>
2024-08-15 23:12:59 -0500

    agregando numeros aleatorios entre 1 y 12

--- init.py
+++ init.py
@@ -1,3 +1,8 @@
+import random
 cars = ["Kia", "Chevrolet", "Nissan", "Toyota"]
 for i in cars:
     print(i)
+
+for x in range(10):
+    random_entero = random.randint(1, 12)
+    print(random_entero)
